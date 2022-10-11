0c0071febd667e0565f473009be746abe34675a5
rzkystwn0 <rzkystwn0@gmail.com>
2022-10-11 23:14:17 +0700

    hero text edit

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,6 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans&family=Exo&display=swap" rel="stylesheet" />
-        <title>NFT MarketPlace</title>
       </Head>
       <body className="bg-[#0D0B0C] h-[2000px]">
         <Main />
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,25 @@
+import Head from "next/head";
 import Button from "../components/Button";
 import NavBar from "../components/NavBar";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>NFT MarketPlace</title>
+      </Head>
       <NavBar />
       <div className="sm:absolute sm:top-0 sm:left-0 sm:right-0 sm:mt-auto mt-36">
         <div className="container sm:flex">
-          <div className="sm:w-6/12 flex items-center  ">
+          <div className="sm:w-6/12 flex items-center mx-3">
             <div className="my-6">
               <h1 className="exo font-bold sm:text-7xl text-3xl text-white leading-10 text-center sm:text-left">
                 Discover Rare Collections Of Art & <span className="bg-gradient-to-r from-[#DF25E1] to-[#5117F4] px-4">NFT’s</span>
               </h1>
               <p className="text-white DM-Sans sm:text-2xl sm:w-auto w-10/12 my-6  text-xs text-center sm:text-left mx-auto ">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. </p>
-              <div className="flex justify-between w-9/12 sm:mx-0 mx-auto sm:w-5/12">
-              <Button linear={true} text="Explore" />
-              <Button text="Create" />
+              <div className="flex justify-between w-10/12 sm:mx-0 mx-auto sm:w-5/12">
+                <Button linear={true} text="Explore" />
+                <Button text="Create" />
               </div>
             </div>
           </div>
